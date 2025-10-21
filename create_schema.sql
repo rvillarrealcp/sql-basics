778a3ae208ea726068eb2188d6b51a7a257a0468
HCRICKET <heinhaung91@gmail.com>
2025-10-21 17:07:33 -0400

    SQL commit

--- create_schema.sql
+++ create_schema.sql
@@ -7,3 +7,25 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+
+CREATE TABLE addresses (
+id             serial PRIMARY KEY,
+line_1         varchar(255) NOT NULL,
+city           varchar(255),
+state          varchar(2),
+zipcode        INT
+);
+
+CREATE TABLE classes (
+  id            serial PRIMARY KEY,
+  name          varchar(255),
+  credits       INT
+);
+
+CREATE TABLE enrollments (
+  id          serial PRIMARY KEY,
+  students_id   INT,
+  class_id      INT,
+  grade         varchar(1)
+);
+
